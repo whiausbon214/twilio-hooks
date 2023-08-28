64e8e38c2967cfb4571e1a46fd31cec1082876d3
whiausbon214 <77340735+whiausbon214@users.noreply.github.com>
2023-08-28 16:05:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
+$text = "Nothing to see here...";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
